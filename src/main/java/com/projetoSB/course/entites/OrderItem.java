34bb9c38acce0bbf6246fef7db1d99ec77126129
Ryan Souza <ryanssebadelhe@gmail.com>
2025-10-23 09:03:15 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/projetoSB/course/entites/OrderItem.java
+++ src/main/java/com/projetoSB/course/entites/OrderItem.java
@@ -26,7 +26,6 @@ public class OrderItem implements Serializable{
 	}
 
 	public OrderItem(Order order, Product product, Integer quantity, Double price) {
-		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
--- src/main/java/com/projetoSB/course/entites/pk/OrderItemPK.java
+++ src/main/java/com/projetoSB/course/entites/pk/OrderItemPK.java
@@ -58,4 +58,6 @@ public class OrderItemPK implements Serializable{
 	
 	
 	
+	
+	
 }
