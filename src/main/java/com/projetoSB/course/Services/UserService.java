f5ec66a6fe9ba4c7fb637c4eca7b63c494387b51
Ryan Souza <ryanssebadelhe@gmail.com>
2025-10-27 09:33:01 -0300

    Exception handling - findById

--- src/main/java/com/projetoSB/course/Services/UserService.java
+++ src/main/java/com/projetoSB/course/Services/UserService.java
@@ -13,6 +13,8 @@ import com.projetoSB.course.Services.exceptions.ResourceNotFoundException;
 import com.projetoSB.course.entites.User;
 import com.projetoSB.course.repositories.UserRepository;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	
@@ -46,9 +48,14 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity,obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity,obj);
+			return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void updateData(User entity, User obj) {
